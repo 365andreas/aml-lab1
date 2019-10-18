074ae86a4eed3464e07e5d4a401b74d956b579ac
Andreas Triantafyllos <andreas.triantafyllos@iohk.io>
2019-10-18 03:30:16 +0200

    Added feature selectors

--- task1.py
+++ task1.py
@@ -9,9 +9,9 @@ from sklearn.model_selection import GridSearchCV, cross_validate
 from sklearn.ensemble import IsolationForest
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import chi2
+from sklearn.feature_selection import chi2, f_regression
 from sklearn.svm import SVC
-from sklearn.feature_selection import RFE
+from sklearn.feature_selection import RFE, RFECV
 from statistics import mean
 
 
@@ -60,16 +60,17 @@ x_train = pd.DataFrame(data=x_train_new, columns=cols)
 
 # Feature selection
 
-select_k_best_classifier = SelectKBest(chi2, k=100)
-x_train_sel = select_k_best_classifier.fit_transform(x_train, y_train)
-'''
+# feature_selector = SelectKBest(chi2, k=200)
+# feature_selector = SelectKBest(f_regression, k=200)
 svc = SVC(kernel="linear", C=1)
-rfe = RFE(estimator=svc, n_features_to_select=400, step=1)
-x_train_sel = rfe.fit_transform(x_train, y_train)
-'''
+feature_selector = RFE(estimator=svc,
+                       n_features_to_select=200, step=10)
+# feature_selector = RFECV(estimator=svc,
+#                          min_features_to_select=20, cv=10, step=1, n_jobs=3)
 
-mask = select_k_best_classifier.get_support()  # list of booleans
-new_features = []  # The list of your K best features
+x_train_sel = feature_selector.fit_transform(x_train, y_train)
+mask = feature_selector.get_support()  # list of booleans
+new_features = []  # The list of your best features
 
 for bool, feature in zip(mask, cols):
     if bool:
@@ -84,27 +85,27 @@ print(x_train)
 # Lasso
 # Ridge
 # TODO: CV to tune parameters
-param_grid = [
-    {'alpha': [1e-3, 1e-2, 1e-1, 1, 10]},
-    # {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
-]
 
-gs = GridSearchCV(Ridge(),
-                  param_grid=param_grid,
-                  scoring=make_scorer(r2_score),
-                  cv=10,
-                  n_jobs=-1, refit=True, return_train_score=True)
-gs.fit(x_train, y_train)
-results = gs.cv_results_
-reg = gs.best_estimator_
+# param_grid = [
+#     {'alpha': [1e-3, 1e-2, 1e-1, 1, 10]},
+#     # {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
+# ]
+# gs = GridSearchCV(Ridge(),
+#                   param_grid=param_grid,
+#                   scoring=make_scorer(r2_score),
+#                   cv=10,
+#                   n_jobs=-1, refit=True, return_train_score=True)
+# gs.fit(x_train, y_train)
+# results = gs.cv_results_
+# reg = gs.best_estimator_
 
-# reg = LinearRegression().fit(x_train, y_train)
+reg = LinearRegression().fit(x_train, y_train)
 print(reg.score(x_train, y_train))
 
 # score = R2 score
-# cv_results = cross_validate(reg, x_train, y_train, cv=10)
-# print(sorted(cv_results.keys()))
-# print(cv_results['test_score'] )
+cv_results = cross_validate(reg, x_train, y_train, cv=10)
+print(sorted(cv_results.keys()))
+print(cv_results['test_score'])
 # print(mean(cv_results['test_score']))
 
 ###################################################################################
